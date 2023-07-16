45f6fc72134543d7b31fd71b898d6e707572161b
eddygulled <edgargulay69@gmail.com>
2023-07-16 22:14:22 +0300

    UI pages linking

--- logbook/urls.py
+++ logbook/urls.py
@@ -4,4 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', logbook_home_view, name='logbook_home'),
+    path('settings/', profile_settings_view, name='logbook_settings'),
+    path('catalog/', logbook_catalog_view, name='logbook_catalog'),
+    path('catalog/<int:logbook_id>/', logbook_detail_view, name='logbook_detail'),
+    path('catalog/<int:logbook_id>/entry/', create_entry_view, name='logbook_entry'),
+    path('catalog/<int:logbook_id>/entry/<int:entry_id>/', update_entry_view, name='logbook_entry'),
 ]
--- logbook/views.py
+++ logbook/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 
 def logbook_home_view(request):
     return render(request, 'logbook/logbook_home.html', {})
-    
+
+
+def profile_settings_view(request):
+    return render(request, 'logbook/logbook_settings.html', {})
+
+def logbook_catalog_view(request):
+    return render(request, 'logbook/logbook_list.html', {})
+
+
+def logbook_detail_view(request, logbook_id):
+    return render(request, 'logbook/logbook_detail.html', {})
+
+
+def logbook_create_view(request):
+    # create new logbook then redirect to logbook_detail
+    pass
+
+def create_entry_view(request, logbook_id):
+    # create new entry then redirect to logbook_detail
+    return render(request, 'logbook/logbook_entry.html', {})
+
+
+def update_entry_view(request, logbook_id, entry_id):
+    # update entry then redirect to logbook_detail
+    return render(request, 'logbook/logbook_entry.html', {})
